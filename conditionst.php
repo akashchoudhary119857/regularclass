cf0791084b0b49448b2a7717637a44fd7f2f13d0
Akash Choudhary <akash.choudhary@gla.ac.in>
2021-02-22 14:23:07 +0530

    new introduction type casting

---
+++ conditionst.php
@@ -0,0 +1,74 @@
+<?php
+$user=8;
+$pass=91;
+$amt=300;
+
+/*if($amt>=300)//433>200
+{
+    echo "go to 3 star restrau";
+}
+else if($amt<300 && $amt>=200){
+    echo "go to food street";
+}
+else if($amt<200 && $amt>=100)
+{
+    echo "go to college canteen";
+}
+else {
+    echo "stay in class ";
+}
+echo "<br> outside if else";
+
+if($user==8 && $pass==9){
+    echo "login successfully";
+}
+else{
+    echo "check your user name and password";
+}
+
+if($user==7)
+{
+    if ($pass==9)
+    {
+        echo "login successfully";
+    }
+    else
+    {
+        echo "wrong password";
+    }
+}
+else
+{
+    if ($pass==9) {
+        echo "wrong username";
+    }
+    else{
+        echo "wrong username and password";
+    }
+}*/
+$day_num=80;
+if($day_num==1){
+    echo "Monday";
+}
+else if($day_num==2){
+    echo "<br> Tuesday";
+}
+else if ($day_num==3){
+    echo "<br> Wednesday";
+}
+else if($day_num==4){
+    echo "<br> Thursday";
+}
+else if ($day_num==5){
+    echo "friday";
+}
+else if($day_num==6){
+    echo "saturday";
+}
+else if($day_num==7){
+    echo "sunday";
+}
+else {
+    echo "wrong input";
+}
+
--- stu.php
+++ stu.php
@@ -1,42 +1,45 @@
 <?php
-$var=NULL;
+$var = NULL;
 var_dump($var);
-$in=455787878458748795456456456;
+$in = 455787878458748795456456456;
 echo "<br>";
 var_dump($in);
-$fl=754.450;
+$fl = 754.450;
 echo "<br>";
 var_dump($fl);
-$str='akash';
+$str = 'akash';
 echo "<br>";
 var_dump($str);
 echo "<br>";
-$arr=array('10.0',5.0,'8.0',7,9.0);
+$arr = array('10.0', 5.0, '8.0', 7, 9.0);
 var_dump($arr);
-class car{
-    function  cars(){
-        $this->model='bmw';
+
+class car
+{
+    function cars()
+    {
+        $this->model = 'bmw';
     }
 }
-$ope=3+5+6+8;
-$vehi=new car();
+
+$ope = 3 + 5 + 6 + 8;
+$vehi = new car();
 echo "<br>";
 var_dump($vehi);
 echo "<br>";
-$bina=(bool)1;
-$a=3;
+$bina = (bool)1;
+$a = 3;
 echo "<br>";
-echo ($a<<4);
-$b=20;
-if($a==3 || $b==200){
+echo($a << 4);
+$b = 20;
+if ($a == 3 || $b == 200) {
     echo "<br>";
     echo "yes";
 }
 
-$x=2;
-$y=4;
-while ($y != 0)
-{
+$x = 2;
+$y = 4;
+while ($y != 0) {
 
     // carry now contains common
     //set bits of x and y
@@ -52,18 +55,17 @@ while ($y != 0)
     $y = $carry << 1;
 }
 echo "<br>";
-echo $x."<br>";
+echo $x . "<br>";
 echo "<br>";
-echo 12|33;
+echo 12 | 33;
 echo "<br>";
-echo 12^33;
+echo 12 ^ 33;
 
-$a=6;
-$b=4;
+$a = 6;
+$b = 4;
 
 
-while ($b != 0)
-{
+while ($b != 0) {
 
     // carry now contains common
     //set bits of x and y
@@ -79,153 +81,160 @@ while ($b != 0)
     $b = $carry << 1;
 }
 echo "<br>";
-echo "<br>".$a;
+echo "<br>" . $a;
 
-$a="akash";
-$b="Php trainer";
+$a = "akash";
+$b = "Php trainer";
 echo "<br>";
-$a = $a.$b;
+$a = $a . $b;
 
-$arr_1=array("1","2","3");
-$arr_2=array("3"=>1,2,3);
+$arr_1 = array("1", "2", "3");
+$arr_2 = array("3" => 1, 2, 3);
 echo "<br>";
 var_dump($arr_1);
 echo "<br>";
 var_dump($arr_2);
 echo "<br>";
-var_dump($arr_2+$arr_1);
+var_dump($arr_2 + $arr_1);
 
-$a=0;
-$b=2;
-if(!$a && $b++)
-{
+$a = 0;
+$b = 2;
+if (!$a && $b++) {
     $a++;
 }
 echo "<br>";
-echo "<br>".$a;
+echo "<br>" . $a;
 
 
-$val_1="akash";
-$val_2="choudhary";
+$val_1 = "akash";
+$val_2 = "choudhary";
 echo "<br>";
 var_dump($val_1);
 echo "<br>";
 var_dump($val_2);
 echo "<br>";
-var_dump($val_1!==$val_2);
+var_dump($val_1 !== $val_2);
 echo "<br>";
-echo 'Current script owner: ' . get_current_user()."\n";
+echo 'Current script owner: ' . get_current_user() . "\n";
 echo "<br>";
-var_dump($val_1<=>$val_2);
-$res=$val_1--;
-echo "<br>".$res;
-echo $val_1."<br>";
+var_dump($val_1 <=> $val_2);
+$res = $val_1--;
+echo "<br>" . $res;
+echo $val_1 . "<br>";
 var_dump(!$val_2);
-$val_2.=$val_1;
-echo "<br>".$val_2."<br>";
-$ar_1=array(1,2.50,3);
-$ar_2=array(1,2,3);
-$secI=array("aditya"=>67,"ankit"=>89);
+$val_2 .= $val_1;
+echo "<br>" . $val_2 . "<br>";
+$ar_1 = array(1, 2.50, 3);
+$ar_2 = array(1, 2, 3);
+$secI = array("aditya" => 67, "ankit" => 89);
 var_dump($ar_1);
 echo "<br>";
 var_dump($ar_2);
 echo "<br>";
-var_dump($ar_2!==$ar_1);
+var_dump($ar_2 !== $ar_1);
 //echo phpinfo();
-$num_1=(bool)"0";
+$num_1 = (bool)"0";
 //$num_2=60;
 echo "<br>";
 //$res=($num_1>$num_2)? :"Null";
 echo $res;
 
-$nc=$num_1??$num_2??null??7;
-echo "<br>".$nc;
-define("greeting",8);
+$nc = $num_1 ?? $num_2 ?? null ?? 7;
+echo "<br>" . $nc;
+define("greeting", 8);
 echo "<br>";
 echo greeting;
 echo "<br>";
- echo constant("greeting");
+echo constant("greeting");
 
- define("seca",["pulsar 150","pulsar 150 double disc","pulsar 150 single disc"]);
+define("seca", ["pulsar 150", "pulsar 150 double disc", "pulsar 150 single disc"]);
 echo "<br>";
- print_r(seca);
+print_r(seca);
 
 echo round(5.5);
 //echo number_format(657841548715,"","","");
 echo "<br>";
-echo "<br>".$nc;
+echo "<br>" . $nc;
 
-$a=boolval(" ");
+$a = boolval(" ");
 var_dump($a);
-if ($a){
+if ($a) {
     echo "yes";
 }
 
-define("pie",3.14);
-echo "<br>".pie;
+define("pie", 3.14);
+echo "<br>" . pie;
 
 echo constant("pie");
 print pie;
-define("Pie",45);
+define("Pie", 45);
 echo Pie;
-$in=34;
-$fl=3.0;
-$res=$in+$fl;
+$in = 34;
+$fl = 3.0;
+$res = $in + $fl;
 echo "<br>";
 var_dump($res);
-$x=2;
-$y=4;
+$x = 2;
+$y = 4;
 echo "<br>";
-var_dump($x/$y);
+var_dump($x / $y);
 echo "<br>";
-var_dump($y/$x);
+var_dump($y / $x);
 // type cast into integer
-$tyin=(int)$fl;
+$tyin = (int)$fl;
 echo "<br>";
 var_dump($tyin);
 
-$str="14 hello456";
-$in=(integer)$str;
+$str = "14 hello456";
+$in = (integer)$str;
 echo "<br>";
 var_dump($in);
 // type caste into float
-$tyf=(float)$x;
+$tyf = (float)$x;
 echo "<br>";
 var_dump($tyf);
 
 //type caste into bool
-$a=(bool)null;
-$b=(bool)'';
+$a = (bool)null;
+$b = (bool)'';
 echo "<br>";
 var_dump($a);
-$b=(bool)'hello';
+$b = (bool)'hello';
 echo "<br>";
 var_dump($b);
-$c=(bool)"my name";
+$c = (bool)"my name";
 echo "<br>";
 var_dump($c);
-$d=(bool)0;
+$d = (bool)0;
 echo "<br>";
 var_dump($d);
-$e=(bool)45;
+$e = (bool)45;
 echo "<br>";
 var_dump($e);
-$f=(bool)4.0;
+$f = (bool)4.0;
 echo "<br>";
 var_dump($e);
-$g=(bool)[];
+$g = (bool)[];
 echo "<br>";
 var_dump($g);
-$h=(bool)[1,2];
+$h = (bool)[1, 2];
 echo "<br>";
 var_dump($h);
-$i=(bool)[1,"hello"];
+$i = (bool)[1, "hello"];
 echo "<br>";
 var_dump($i);
 //type caste into an array
-$b=(array)true;
+$b = (array)true;
 echo "<br>";
 var_dump($b);
+
+$original_string="qwertyuiopasdfghjklzxcvbnm1234567890MNBVCXZLKJHGFDSAPOIUYTREWQ";
+$ret_string=substr(str_shuffle("$original_string"),26,15);
+echo "<br>".$ret_string;
+
+$extra_chr='\"1+2/3*2:2-3/4*3/7*2';
+echo "<br>".str_replace(str_split('\"+*:-/'),'',$extra_chr);
+
 
 
 
